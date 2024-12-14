387b24c3d2d6bb08abb524823c86f1a117eac398
appu-kumar-tealfeed <appu@tealfeed.com>
2024-12-14 22:14:08 +0530

    add the authentication

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,81 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnect";
+import userModel from "@/model/User";
+
+
+export const authOptions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "text", placeholder: "Appu Kumar" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials:any): Promise<any> {
+        await dbConnect();
+        try {
+          const user = await userModel.findOne({
+            $or: [
+              { email: credentials.identifier.email },
+              { password: credentials.identifier.password },
+            ],
+          });
+
+          if (!user) {
+            throw new Error("No user found with this email");
+          }
+
+          if (!user.isVerified) {
+            throw new Error("Please verify your account first before login ");
+          }
+
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.identifier.password,
+            user.password
+          );
+
+          if (isPasswordCorrect) {
+            return user;
+          } else {
+            throw new Error("Please verify your account first before login ");
+          }
+        } catch (error: any) {
+          throw new Error(error);
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      if (token) {
+        session.user._id = token._id;
+        session.user.isVerified = token.isVerified;
+        session.user.isAcceptingMessages = token.isAcceptingMessages;
+        session.user.username = token.username;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString();
+        token.isVerified = user.isVerified;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+        token.username = user.username;
+      }
+      return token;
+    },
+  },
+  pages: {
+    // automatically will create the pages.
+    signIn: "/sign-in",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+export { default } from "next-auth/middleware"; // this makes the pages secure with auth, following are the paths that are the secured with auth.
+import { getToken } from "next-auth/jwt";
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
+
+  if (
+    token &&
+    (url.pathname.startsWith("/sign-in") ||
+      url.pathname.startsWith("/sign-up") ||
+      url.pathname.startsWith("/") ||
+      url.pathname.startsWith("/verify"))
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  // applies middleware to this path
+  matcher: ["/sign-in", "/sign-up", "/", "/dashboard/:path*", "/verify"],
+};
